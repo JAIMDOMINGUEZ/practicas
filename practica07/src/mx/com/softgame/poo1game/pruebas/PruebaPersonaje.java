d16ea5c4c785c74b569fbfaf48fc29b50c30bad0
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-02-26 08:55:24 -0600

    PASO 6
    6

--- practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ practica07/src/mx/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -11,26 +11,35 @@ public class PruebaPersonaje{
        
         Personaje per04 = new Personaje("Lesly");
         
-        for(int i =0;i<3;i++){
-            ed = (int)Math.random()*250),
+        for(int i=0;i<3;i++){
+            ed = (int)Math.random()*250);
             if(per01.setEdad(ed){
                 System.out.println("Omar"+per01.getDetalle()+"se modifico la edad");
             }else:{
-                System.out.println("Omar"+per01.getDetalle()+"sin modificacion de edad");
+                System.out.println("Omar"+per01.getDetalle()+"sin modificacion de edad a "+ed);
             }
+            ed = (int)Math.random()*250);
             if(per02.setEdad(ed)){
                 System.out.println("Erick"+per02.getDetalle()+"se modifico la edad");
             }else:{
-                System.out.println("Erick"+per02.getDetalle()+"sin modificacion de edad");
+                System.out.println("Erick"+per02.getDetalle()+"sin modificacion de edad a "+ed);
             }
+            ed = (int)Math.random()*250);
              if(per03.setEdad(ed)){
-                System.out.println("Wendy"+per03.getDetalle()+"se modifico la edad");
+                System.out.println("Wendy"+per03.getDetalle()+"se modifico la edad a "+ed);
             }else:{
-                System.out.println("Wendy"+per03.getDetalle()+"sin modificacion de edad");
+                System.out.println("Wendy"+per03.getDetalle()+"sin modificacion de edad a "+ed);
             }
             do{
+                ed = (int)Math.random()*250);
+                System.out.println(ed);
+                if(ed ==150){
+                    System.out.println("**Bingo 150**")
+                    continue outfor;
+                }
 
-            }while(!)
+            }while(!per04.setEdad(ed));
+            System.out.println(per04.getDetalle());
 
             
                 
