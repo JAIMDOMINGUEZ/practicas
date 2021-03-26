a932839f2dc36a7d51a7b33d28fcff947a07ab94
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-03-26 08:33:00 -0600

    paso 5 8:32
    Creacion de clase tablero

---
+++ practica11/src/mx/com/softgame/poo1game/Utils/Tablero.java
@@ -0,0 +1,15 @@
+package mx.com.softgame.poo1game.Utils;
+import mx.com.softgame.poo1game.personajes.Personaje;
+public class Tablero {
+    static int MAX_LINE;
+    private static int  idx;
+    static private Personaje personajes = new Personaje [MAX_LINE];
+    private Tablero(int MAX_LINE,int idx){
+        this.MAX_LINE = MAX_LINE;
+        this.idx = idx;
+    }
+    static boolean addPersonaje(Personaje p){
+
+
+    }
+}
