e72abda8e6ec7a18e08169c85a640cb6eab1a0bc
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-16 08:04:34 -0500

    Paso 4 8:04
    Creacion de clase PruebTableroE

--- practica12/src/mx/com/softgame/poo1game/utils/Tablero.java
+++ practica12/src/mx/com/softgame/poo1game/utils/Tablero.java
@@ -1,14 +1,17 @@
 package mx.com.softgame.poo1game.utils;
 import mx.com.softgame.poo1game.personajes.Personaje;
+import  mx.com.softgame.poo1game.utils.TheException;
 public class Tablero {
     static int MAX_LINE = 10;
     private static int  idx= -1;
     static private Personaje[] personajes = new Personaje [MAX_LINE];
+    static TheException theExceptionObj = new TheException("",0);
     private Tablero(int MAX_LINE,int idx){
         this.MAX_LINE = MAX_LINE;
         this.idx = idx;
     }
     public static void addPersonaje(Personaje p){
+        
         boolean temp = false;
         try {
             if(idx >= 0 ){
@@ -17,7 +20,7 @@ public class Tablero {
                 
             } 
         } catch (Exception e) {
-            
+            System.out.println("No es posible insetar el personaje"+theExceptionObj.getPoss());
         }
         
         
@@ -25,11 +28,16 @@ public class Tablero {
     }
     public static void delPersonaje(Personaje p){
         boolean temp = false;
-        if((idx- 1)>= 0 && (idx-1) < MAX_LINE){
-            idx--;
-            personajes[idx]=null;
-            
+        try {
+            if((idx- 1)>= 0 && (idx-1) < MAX_LINE){
+                idx--;
+                personajes[idx]=null;
+                
+            }
+        } catch (Exception e) {
+            System.out.println("No es posible borrar el personaje"+theExceptionObj.getPoss());
         }
+        
         
         
     }
