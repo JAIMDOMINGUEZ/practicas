83e51d4d3138c200c09320ae8310ba0c983f7ba2
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-23 10:07:36 -0500

    Termine 10:07

--- practica13/src/mx/com/softgame/poo1game/utils/SortId.java
+++ practica13/src/mx/com/softgame/poo1game/utils/SortId.java
@@ -5,7 +5,9 @@ import mx.com.softgame.poo1game.personajes.Personaje.*;
 public class SortId implements Comparator<Personaje>{
     public int compare(Personaje o1,Personaje o2){
         return o1.getId()- o2.getId();
+        
     }
+
 
     
 
