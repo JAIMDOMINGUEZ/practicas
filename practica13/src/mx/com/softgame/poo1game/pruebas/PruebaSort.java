b4785e39099748cdd9fbd74315ce9b6187fecd5d
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-23 10:07:04 -0500

    Paso 9 10:07
    Correccion de nota: se agrego generics para especificar de que tipo es la comparacion

--- practica13/src/mx/com/softgame/poo1game/pruebas/PruebaSort.java
+++ practica13/src/mx/com/softgame/poo1game/pruebas/PruebaSort.java
@@ -12,6 +12,7 @@ import mx.com.softgame.poo1game.utils.SortId;
 public class PruebaSort {
     public static void main(String[]args){
         TreeSet <Personaje>ts = new TreeSet<Personaje>();
+        
         ts.add(new Planta("Cristian",70));
         ts.add(new Planta("Karen",50));
         ts.add(new Zombie("Hanna",80,false));
@@ -22,9 +23,15 @@ public class PruebaSort {
         ts.add(new Zombie("Cristian",80,false));
         ts.add(new Zombie("Miguel",80,false));
         ts.add(new Planta("Karen",50));
+        
         for(Personaje p:ts){
             System.out.println(p);
+            
         }
+        
+            
+        
+       
         System.out.println("************");
         LinkedList<Personaje> ls= new LinkedList<Personaje>();
         ls.add(new Planta("Cristian",70));
--- practica13/src/mx/com/softgame/poo1game/utils/SortId.java
+++ practica13/src/mx/com/softgame/poo1game/utils/SortId.java
@@ -2,9 +2,9 @@ package mx.com.softgame.poo1game.utils;
 import java.util.Comparator;
 import mx.com.softgame.poo1game.personajes.Personaje;
 import mx.com.softgame.poo1game.personajes.Personaje.*;
-public class SortId implements Comparator{
-    public int compare(Object o1,Object o2){
-        return ((Personaje) o1).getId()-((Personaje) o2).getId();
+public class SortId implements Comparator<Personaje>{
+    public int compare(Personaje o1,Personaje o2){
+        return o1.getId()- o2.getId();
     }
 
     
