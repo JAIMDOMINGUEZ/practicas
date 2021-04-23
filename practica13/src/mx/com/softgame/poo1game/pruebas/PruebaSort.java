eb28378c67726d25b1a31fa32bb83a5a49dd73d2
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-23 07:59:29 -0500

    Paso 4 A 7:59
    Se creo un TreeSet

--- practica13/src/mx/com/softgame/poo1game/pruebas/PruebaSort.java
+++ practica13/src/mx/com/softgame/poo1game/pruebas/PruebaSort.java
@@ -1,5 +1,25 @@
 package mx.com.softgame.poo1game.pruebas;
+import java.util.TreeSet;
 
+import mx.com.softgame.poo1game.personajes.Personaje;
+import mx.com.softgame.poo1game.personajes.buenos.Planta;
+import mx.com.softgame.poo1game.personajes.malos.Zombie;
 public class PruebaSort {
-    
+    public static void main(String[]args){
+        TreeSet ts = new TreeSet();
+        ts.add(new Planta("Cristian",70));
+        ts.add(new Planta("Cristian",70));
+        ts.add(new Zombie("Hanna",80,false));
+        ts.add(new Zombie("Erick",false));
+        ts.add(new Zombie("Esmeralda",80,false));
+        ts.add(new Planta("Miguel",99));
+        ts.add(new Planta("Jaime",50));
+        ts.add(new Zombie("Cristian",80,false));
+        ts.add(new Zombie("Miguel",80,false));
+        ts.add(new Planta("Karen",50));
+        for(Personaje p : ts){
+            System.out.println();
+        }
+    }
+   
 }
--- practica13/src/mx/com/softgame/poo1game/utils/SortId.java
+++ practica13/src/mx/com/softgame/poo1game/utils/SortId.java
@@ -1,5 +1,9 @@
 package mx.com.softgame.poo1game.utils;
+import java.util.Comparator;
 
-public class SortId {
-    
+import mx.com.softgame.poo1game.personajes.Personaje;
+public class SortId implements Comparator<Personaje>{
+    public int compare(Personaje p1,Personaje p2){
+        return p1.getId()-p2.getId();
+    }
 }
