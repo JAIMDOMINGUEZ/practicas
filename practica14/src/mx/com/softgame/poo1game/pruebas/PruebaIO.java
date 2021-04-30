884ebecc613c938db1a67670aa6ce7adc5a3b78d
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-30 08:54:04 -0500

    paso 7 8:53
    Crear arrray list

--- practica14/src/mx/com/softgame/poo1game/pruebas/PruebaIO.java
+++ practica14/src/mx/com/softgame/poo1game/pruebas/PruebaIO.java
@@ -1,14 +1,28 @@
+package mx.com.softgame.poo1game.pruebas;
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileReader;
+import java.nio.Buffer;
 public class PruebaIO {
     public static void main(String[] args) {
        Scanner file = new  Scanner(System.in);
-       String fle = file.nextline();
+       System.out.println("nombre del archivo:");
+       String fle = file.nextLine();
        String path = System.getProperty("user.home");
-       path += "\\"+fle.trim();
-       System.Out.println(path) ;
-       File myFile = new File("fle");
-      
+       path += System.getProperty("file.separator")+fle.trim();
+       System.out.println(path) ;
+       File archivo = new File(path);
+        
+       if (archivo.exists()) {
+            System.out.println();
+            ArrayList<Personaje> personajes = new ArrayList<Personaje>();
+            FileReader fr =new FileReader();
+            BufferReader br = new Bufferreader();
+      }else{
+        System.out.println("El archivo no existe,fin del programa");
+
+       }
+
 
         
    } 
