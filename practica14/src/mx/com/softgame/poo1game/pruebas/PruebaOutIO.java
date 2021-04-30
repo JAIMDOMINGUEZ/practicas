8d1f311f03d7feba4047eb400abaab1b672adf74
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-04-30 11:37:47 -0500

    Paso 13 10:37
    Error no imprime todo el archivo

--- practica14/src/mx/com/softgame/poo1game/pruebas/PruebaOutIO.java
+++ practica14/src/mx/com/softgame/poo1game/pruebas/PruebaOutIO.java
@@ -1,23 +1,41 @@
 package mx.com.softgame.poo1game.pruebas;
-
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInput;
 import java.io.ObjectInputStream;
-import java.util.Date;
+
+import mx.com.softgame.poo1game.personajes.Personaje;
+
+
+
 
 public class PruebaOutIO {
-    String npath = System.getProperty("user.home")+System.getProperty("file.separator")+"pvsz.out";
-    try {
-        InputStream file = new FileInputStream(npath);
-        InputStream buffer = new BufferedInputStream(file);
-        ObjectInput input = new ObjectInputStream(buffer);
+    private static String npath = System.getProperty("user.home")+System.getProperty("file.separator")+"pvsz.out";
+     public static void main(String[] args) {
+        Personaje pe;
+        try {
+            FileInputStream f =new FileInputStream(npath);
+            ObjectInputStream s =new ObjectInputStream (f);
+           
+            while(s.readObject()!=null)
+                {
+                    pe= (Personaje)s.readObject();
+                    System.out.println(pe);
+                 
+                }
+            s.close ();
+            
+        }catch(Exception e) {
+           
+        }
+        
+        
+    
        
-          
-    } catch (Exception e) {
-        ex.printStackTrace();
+         
+        
     }
     
-
-   
-    }
     
 }
