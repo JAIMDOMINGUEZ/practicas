cde4f1d1d34b96386a780e745ccc6d3ec0a84ed0
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-05-07 07:41:10 -0500

    paso 4 7:40
    crear constructor

--- practica14/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica14/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -7,6 +7,12 @@ public class VentanaPrincipal {
     JTextField txtFil;
     JButton btnOpen;
     JTextArea txtCont;
-
-     
+    Jlabel lblN;
+    JLabel lblLeidos;
+    Jbutton btnExit;
+   public VentanaPrincipal(){
+    f = new Jframe();
+    f.setTitle("Practica 14");
+    lblFile = new Jlabel();
+   } 
 }
