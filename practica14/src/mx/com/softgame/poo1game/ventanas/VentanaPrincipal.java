b34a44ba6a72fad87e9ca219997ff5177e607911
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-05-07 07:55:25 -0500

    Paso 5 7:55
    Crear metodo initComponents

--- practica14/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica14/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,4 +1,6 @@
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class VentanaPrincipal {
@@ -13,6 +15,17 @@ public class VentanaPrincipal {
    public VentanaPrincipal(){
     f = new Jframe();
     f.setTitle("Practica 14");
-    lblFile = new Jlabel();
+    lblFile = new Jlabel("Nombre del arhivo");
+    txtFil = new JTextField();
+    btnOpen = JButton("Abrir archivo");
+    txtCont = JTextArea();
+    txtCont.setColumns(40);
+    txtCont.setRows(30);
+    lblLeidos = new JLabel("Caracteres Leidos");
+    lblN = new Jlabel("0");
+    btnExit = new JButton("Salir");
    } 
+   private void initComponents() {
+       
+   }
 }
