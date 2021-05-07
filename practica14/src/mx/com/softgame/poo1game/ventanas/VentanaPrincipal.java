97515ce77ee2eba4ae3572d30021590a1909543c
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-05-07 07:36:32 -0500

    paso 4 7:36
    Crear constructor

---
+++ practica14/src/mx/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -0,0 +1,12 @@
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VentanaPrincipal {
+    JFrame f;
+    JLabel lblFile;
+    JTextField txtFil;
+    JButton btnOpen;
+    JTextArea txtCont;
+
+     
+}
