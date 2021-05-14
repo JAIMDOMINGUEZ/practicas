fdb48325c8e30a73b68ec0b65168ac8fdc3e3ad9
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-05-14 08:16:11 -0500

    Paso 4 08:16
    Crear metodo initcomponents

---
+++ practica16/src/mx/com/softgame/poo1game/ventanas/VentanaHilo.java
@@ -0,0 +1,16 @@
+package mx.com.softgame.poo1game.ventanas;
+
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class VentanaHilo {
+    JMenuBar menubar;
+    JLabel lblAtextoA;
+    JLabel lblAtextoB;
+    JLabel lblAtextoC;
+    JLabel lblAtextoD;
+    JMenu mnuArchivotextoArchivo;
+    JMenuItem menuiniciotextoinicio;
+    JMenuItem mnuSalirtextoSalir;    
+}
