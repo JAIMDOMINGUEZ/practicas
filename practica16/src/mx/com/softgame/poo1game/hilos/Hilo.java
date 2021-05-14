f2eadb3cfb474f2947406950e56e7dee88d68c57
JAIMDOMINGUEZ <79319348+JAIMDOMINGUEZ@users.noreply.github.com>
2021-05-14 08:10:18 -0500

    paso 3 8:10
    Crear la clase VentanaHilo

--- practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
+++ practica16/src/mx/com/softgame/poo1game/hilos/Hilo.java
@@ -1,4 +1,26 @@
 package  mx.com.softgame.poo1game.hilos;
+
+
+
+import javax.swing.JLabel;
+
 public class Hilo extends Thread{
-    
+    JLabel etiqueta;
+    public Hilo(String name,Jlabel etiqueta){
+        name = "";
+        etiqueta = new JLabel("");
+    }
+    public void run(){
+        String str;
+        for (int i = 0; i < 10; i++) {
+            str = etiqueta.getText();
+            char c = str.charAt(str.length()-1);
+            etiqueta.setText(str);
+            int n = Math.random()*1000+0;
+            str.sleep(n);
+        }
+        str = str+"Hecho";
+        etiqueta.setText(str);
+
+    }
 }
